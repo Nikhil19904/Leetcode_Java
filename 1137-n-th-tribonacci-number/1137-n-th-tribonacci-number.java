28c4eac1cb1e18b69f7db29060569b4b22bb5fa8
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-05-21 16:14:23 +0530

    Time: 0 ms (100%), Space: 41.7 MB (95.98%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,21 +1,24 @@
 class Solution {
     public int tribonacci(int n) {
 
-        int []dp=new int[n+1];
-        Arrays.fill(dp,-1);
-
-        return solve(n,dp);
-    }
-
-    private int solve(int n,int[]dp){
-
+        // edge cases
         if(n==0) return 0;
         if(n==1 || n==2) return 1;
 
-        if(dp[n]!=-1){
-            return dp[n];
+        // dp array
+        int[]dp=new int[n+1];
+
+        // bases case
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+
+        // fill bottom
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         }
 
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
+        return dp[n];
+        
     }
 }
