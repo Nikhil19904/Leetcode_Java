47e71366ec414fd29e3a860b393965769cd93943
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-05-21 15:22:52 +0530

    Time: 0 ms (100%), Space: 42.2 MB (34.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,41 @@
 class Solution {
     public int climbStairs(int n) {
+        
+    //     // dp array initialization
+    //     int[]dp=new int[n+1];
+  
+    //     // initialize with -1
+    //     Arrays.fill(dp,-1);
 
-        int[]dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n,dp);
+    //     // start recursion
+    //     return solve(n,dp);
+    // }
+
+    // private int solve(int n,int[]dp){
+
+    //     if(n==0 || n==1){
+    //         return 1;
+    //     }
+
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+
+    //     return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    // }
+
+    // dp array
+    int[]dp=new int[n+1];
+
+    // base case
+    dp[0]=1;
+    dp[1]=1;
+
+    // fill table bottom-up
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
     }
 
-    private int solve(int n,int[]dp){
-
-        if(n==0 || n==1){
-            return 1;
-        }
-
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-    }
+    return dp[n];
+}
 }
