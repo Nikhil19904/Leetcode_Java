13edf1319e07836ad97faa56fabe0a80619bafae
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-05-22 21:46:25 +0530

    Time: 7 ms (69.09%), Space: 46.3 MB (81.75%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public int minDistance(String word1, String word2) {
+
+        int n = word1.length();
+        int m = word2.length();
+
+        // dp array
+        int[][] dp = new int[n][m];
+
+        // initialize with -1
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        int lcs = solve(word1, word2, 0, 0, dp);
+
+        return (n - lcs) + (m - lcs);
+    }
+
+    private int solve(String s1, String s2,
+                      int i, int j,
+                      int[][] dp) {
+
+        // base case
+        if (i == s1.length() || j == s2.length()) {
+            return 0;
+        }
+
+        // already computed
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        // characters match
+        if (s1.charAt(i) == s2.charAt(j)) {
+
+            return dp[i][j] =
+                1 + solve(s1, s2, i + 1, j + 1, dp);
+        }
+
+        // characters don't match
+        return dp[i][j] = Math.max(
+            solve(s1, s2, i + 1, j, dp),
+            solve(s1, s2, i, j + 1, dp)
+        );
+    }
+}
