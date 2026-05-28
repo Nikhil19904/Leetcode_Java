9666331629e3071f408302d948bd9f951710327b
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-05-28 23:58:02 +0530

    Time: 5 ms (5.02%), Space: 50.2 MB (5.95%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,44 +1,22 @@
 class Solution {
-    int[][]dp;
+    public int maxProfit(int[] prices) {
 
-    // recursion code
-    public int solveRec(int i ,int buy, int[]prices){
+        int n=prices.length;
 
-        // base case
-        if(i==prices.length){
-            return 0;
+        // dp array
+        int[][]dp=new int[n][2];
+
+        // initialized with -1
+         for(int[] row:dp) {
+            Arrays.fill(row,-1);
         }
 
-        // if can buy the stock
-        if(buy==1){
 
-            // option-1: buy the stock
-            int Buykaro= -prices[i]+solveRec(i+1,0,prices);
-
-            // option2 : skip the stock
-            int SkipKaro=solveRec(i+1,1,prices);
-
-            return Math.max(Buykaro,SkipKaro);
-        }
-
-        // if holding stock(must sell or hold)
-        else{
-
-            // option-1: sell the stock
-            int SellKaro=prices[i]+solveRec(i+1,1,prices);
-
-            // option-2:hold the stock
-            int holdKaro=solveRec(i+1,0,prices);
-
-            return Math.max(SellKaro,holdKaro);
-
-        }
-
+        return solve(0,1,prices,dp);
+        
     }
 
-    // Memoization code
-
-    public int solveMemo(int i, int buy, int[]prices){
+    private int solve(int i,int buy,int[]prices,int[][]dp){
 
         // base case
         if(i==prices.length){
@@ -50,75 +28,25 @@ class Solution {
             return dp[i][buy];
         }
 
-        int result;
+        int profit=0;
 
-        // if buy the stock
+        // buy state
         if(buy==1){
 
-            int BuyKaro=-prices[i] + solveMemo(i + 1, 0, prices);
-            int SkipKaro=solveMemo(i + 1, 1, prices);
+            int buyStock=-prices[i]+solve(i+1,0,prices,dp);
+            int skip=solve(i+1,1,prices,dp);
 
-             result = Math.max(BuyKaro, SkipKaro);
+            profit= Math.max(buyStock,skip);
         }
 
-         // If holding stock
-        else {
+        // sell state
+        else{
+            int sellStock=prices[i]+solve(i+1,1,prices,dp);
+            int hold=solve(i+1,0,prices,dp);
 
-            int SellKaro = prices[i] + solveMemo(i + 1, 1, prices);
-            int holdKaro = solveMemo(i + 1, 0, prices);
-
-            result = Math.max(SellKaro, holdKaro);
+            profit= Math.max(sellStock,hold);
         }
 
-        return dp[i][buy] = result;
-    }
-
-    // tabulation code
-
-    public int solveTab(int[] prices) {
-
-        int n = prices.length;
-
-        int[][] dp = new int[n + 1][2];
-
-        // base case already 0
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            // can buy
-            dp[i][1] = Math.max(
-                -prices[i] + dp[i + 1][0],
-                dp[i + 1][1]
-            );
-
-            // holding stock
-            dp[i][0] = Math.max(
-                prices[i] + dp[i + 1][1],
-                dp[i + 1][0]
-            );
-        }
-
-        return dp[0][1];
-    }
-
-
-
-    public int maxProfit(int[] prices) {
-
-        int n=prices.length;
-
-        // dp array
-        dp= new int[n][2];
-
-        for(int[]row:dp){
-            Arrays.fill(row,-1);
-        }
-
-        // return solveMemo(0,1,prices);
-
-        return solveTab(prices);
-
-        // return solveRec(0,1,prices);
-        
+        return dp[i][buy]=profit;
     }
 }
