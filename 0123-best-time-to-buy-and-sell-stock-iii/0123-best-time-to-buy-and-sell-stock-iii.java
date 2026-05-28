8983a81c67290f605ba65e539243958902857e4d
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-05-29 00:22:47 +0530

    Time: 135 ms (12.05%), Space: 134.4 MB (16.24%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,42 +1,49 @@
 class Solution {
-
-    int[][][] dp;
-
     public int maxProfit(int[] prices) {
 
-        int n = prices.length;
-        dp = new int[n][2][3]; // cap = 2 → 0..2
+        int n=prices.length;
 
-        for (int[][] row : dp)
-            for (int[] r : row)
-                java.util.Arrays.fill(r, -1);
+        // dp array
+        int[][][]dp=new int[n][2][3];
 
-        return solve(0, 1, 2, prices);
-    }
-
-    public int solve(int i, int buy, int cap, int[] prices) {
-
-        if (i == prices.length || cap == 0) return 0;
-
-        if (dp[i][buy][cap] != -1) return dp[i][buy][cap];
-
-        int ans;
-
-        if (buy == 1) {
-
-            int take = -prices[i] + solve(i + 1, 0, cap, prices);
-            int skip = solve(i + 1, 1, cap, prices);
-
-            ans = Math.max(take, skip);
-
-        } else {
-
-            int sell = prices[i] + solve(i + 1, 1, cap - 1, prices);
-            int hold = solve(i + 1, 0, cap, prices);
-
-            ans = Math.max(sell, hold);
+        // initialized with -1
+        for(int[][]mat:dp){
+            for(int[]row:mat){
+            Arrays.fill(row,-1);
+            }
         }
 
-        return dp[i][buy][cap] = ans;
+        return solve(0,1,2,prices,dp);
+        
+    }
+
+    private int solve(int i,int buy,int cap,int[]prices,int[][][]dp){
+
+        if(i==prices.length || cap==0){
+            return 0;
+        }
+
+        if(dp[i][buy][cap]!=-1){
+            return dp[i][buy][cap];
+        }
+
+        int profit=0;
+
+        if(buy==1){
+
+            int buyStock=-prices[i]+solve(i+1,0,cap,prices,dp);
+            int skip=solve(i+1,1,cap,prices,dp);
+
+            profit= Math.max(buyStock,skip);
+        }
+
+        else{
+            int sellStock=prices[i]+solve(i+1,1,cap-1,prices,dp);
+            int hold=solve(i+1,0,cap,prices,dp);
+
+            profit= Math.max(sellStock,hold);
+        }
+
+        return dp[i][buy][cap]=profit;
     }
 }
