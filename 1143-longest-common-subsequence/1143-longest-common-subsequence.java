0704e82a7b3de04236ef1b42855800f471403b79
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-06-02 01:04:44 +0530

    Time: 27 ms (21.86%), Space: 53.9 MB (50.2%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -11,10 +11,10 @@ class Solution {
             Arrays.fill(row,-1);
         }
 
-        return solve(text1,text2,0,0,dp);
+        return solveMemo(text1,text2,0,0,dp);
     }
 
-    private int solve(String s1, String s2, int i,int j,int[][]dp){
+    private int solveMemo(String s1, String s2, int i,int j,int[][]dp){
 
         // base case
         if(i==s1.length() || j==s2.length()){
@@ -28,10 +28,10 @@ class Solution {
 
         // characters match
         if(s1.charAt(i)==s2.charAt(j)){
-            return dp[i][j]=1 + solve(s1,s2,i+1,j+1,dp);
+            return dp[i][j]=1 + solveMemo(s1,s2,i+1,j+1,dp);
         }
 
         // characeters don't match
-        return dp[i][j]=Math.max(solve(s1,s2,i+1,j,dp),solve(s1,s2,i,j+1,dp));
+        return dp[i][j]=Math.max(solveMemo(s1,s2,i+1,j,dp),solveMemo(s1,s2,i,j+1,dp));
     }
 }
