eaa3278b5176f4cdebd79623f56e0027b181ac3f
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-06-02 02:32:51 +0530

    Time: 8 ms (22.1%), Space: 46.4 MB (45.91%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -4,22 +4,52 @@ class Solution {
         int n=word1.length();
         int m=word2.length();
 
-        int [][]dp=new int[n][m];
+        // dp array
+        int[][]dp=new int[n][m];
 
+        // fill the array
         for(int []row:dp){
             Arrays.fill(row,-1);
         }
-         return solve(word1, word2, 0, 0,dp);
+
+        // return solveRec(word1,word2,0,0);
+        return solveMemo(word1,word2,0,0,dp);
     }
 
-    private int solve(String s1, String s2,int i,int j,int[][]dp){
+    private int solveRec(String s1, String s2, int i,int j){
 
-        // s1 exhausted
+        // word1 khatam
         if(i==s1.length()){
             return s2.length()-j;
         }
 
-        // s2 exhausted
+        // word2 khatam
+        if(j==s2.length()){
+            return s1.length()-i;
+        }
+
+        // chars match
+        if(s1.charAt(i)==s2.charAt(j)){
+            return solveRec(s1,s2,i+1,j+1);
+        }
+
+        // delete from first string
+        int deleteFromS1=1+solveRec(s1,s2,i+1,j);
+
+        // delete from second string
+        int deleteFromS2=1+solveRec(s1,s2,i,j+1);
+
+        return Math.min(deleteFromS1,deleteFromS2);
+    }
+
+    // memoization
+    private int solveMemo(String s1,String s2,int i,int j,int[][]dp){
+
+        // base case
+        if(i==s1.length()){
+            return s2.length()-j;
+        }
+
         if(j==s2.length()){
             return s1.length()-i;
         }
@@ -28,18 +58,24 @@ class Solution {
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-
-        // characters match
+        
+        
+        // chars match
         if(s1.charAt(i)==s2.charAt(j)){
-            return dp[i][j]=solve(s1,s2,i+1,j+1,dp);
+            return dp[i][j]=solveMemo(s1,s2,i+1,j+1,dp);
         }
 
-        // delete from s1
-        int deleteS1=1+solve(s1,s2,i+1,j,dp);
+        
+        // delete from first string
+        int deleteFromS1=1+solveMemo(s1,s2,i+1,j,dp);
 
-        // delete from s2
-        int deleteS2=1+solve(s1,s2,i,j+1,dp);
+        // delete from second string
+        int deleteFromS2=1+solveMemo(s1,s2,i,j+1,dp);
 
-        return dp[i][j]=Math.min(deleteS1,deleteS2);
+
+        return dp[i][j]=Math.min(deleteFromS1,deleteFromS2);
+
     }
+
+
 }
