8f388025985bce53add3648de49d36c1e3dabd9e
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-06-02 00:19:08 +0530

    Time: 30 ms (22.1%), Space: 53.2 MB (5.29%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -4,10 +4,17 @@ class Solution {
         int n=s.length();
         int count=0;
 
+        // dp array
+        int[][]dp=new int[n][n];
+
+        for(int[]row:dp){
+            Arrays.fill(row,-1);
+        }
+
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
 
-                if(isPalindrome(s,i,j)){
+                if(isPalindrome(s,i,j,dp)){
                     count++;
                 }
             }
@@ -17,19 +24,30 @@ class Solution {
         
     }
 
-    private boolean isPalindrome(String s,int i,int j){
+    private boolean isPalindrome(String s,int i,int j,int[][]dp){
 
         // base case
         if(i>=j){
             return true;
         }
 
+        // alraedy computed
+        if(dp[i][j]!=-1){
+            return dp[i][j]==1;
+        }
+
         // characters don't match
         if(s.charAt(i)!=s.charAt(j)){
+
+            dp[i][j]=0;
             return false;
         }
 
         // check inner substring
-        return isPalindrome(s,i+1,j-1);
+        boolean ans= isPalindrome(s,i+1,j-1,dp);
+
+        dp[i][j]=ans?1:0;
+
+        return ans;
     }
 }
