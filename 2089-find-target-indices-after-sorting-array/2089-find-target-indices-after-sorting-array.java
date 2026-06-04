3246ae6076dca3cc133d27af9eecb9c24e8dfb8f
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-06-04 21:56:19 +0530

    Time: 7 ms (24.82%), Space: 50.7 MB (7.38%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,13 +1,58 @@
 class Solution {
+
     public List<Integer> targetIndices(int[] nums, int target) {
-        List<Integer>list=new ArrayList<>();
+
         Arrays.sort(nums);
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                list.add(i);
+        List<Integer> ans = new ArrayList<>();
+
+        int start = lowerBound(nums, target);
+        int end = upperBound(nums, target) - 1;
+
+        for (int i = start; i <= end; i++) {
+            ans.add(i);
+        }
+
+        return ans;
+    }
+
+    private int lowerBound(int[] nums, int target) {
+
+        int low = 0, high = nums.length - 1;
+        int ans = nums.length;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return list;
+
+        return ans;
+    }
+
+    private int upperBound(int[] nums, int target) {
+
+        int low = 0, high = nums.length - 1;
+        int ans = nums.length;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (nums[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
     }
 }
