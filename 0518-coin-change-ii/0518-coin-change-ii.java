0a765f2346c65d4cfb215c9626fe4a37f5907175
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-06-05 23:09:48 +0530

    Time: 27 ms (18.1%), Space: 56.9 MB (35.3%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,8 +1,12 @@
+import java.util.*;
+
 class Solution {
 
     public int change(int amount, int[] coins) {
 
-        int[][] dp = new int[coins.length][amount + 1];
+        int n = coins.length;
+
+        int[][] dp = new int[n][amount + 1];
 
         for (int[] row : dp) {
             Arrays.fill(row, -1);
@@ -11,30 +15,32 @@ class Solution {
         return solve(0, amount, coins, dp);
     }
 
-    private int solve(int idx, int amount, int[] coins, int[][] dp) {
+    private int solve(int i, int amount, int[] coins, int[][] dp) {
 
-        // amount formed
-        if (amount == 0)
+        // Base Case
+        if (amount == 0) {
             return 1;
-
-        // no coins left
-        if (idx == coins.length)
-            return 0;
-
-        // already computed
-        if (dp[idx][amount] != -1)
-            return dp[idx][amount];
-
-        int notTake = solve(idx + 1, amount, coins, dp);
-
-        int take = 0;
-
-        if (coins[idx] <= amount) {
-
-            // same idx because unlimited supply
-            take = solve(idx, amount - coins[idx], coins, dp);
         }
 
-        return dp[idx][amount] = take + notTake;
+        if (i == coins.length) {
+            return 0;
+        }
+
+        // Already Computed
+        if (dp[i][amount] != -1) {
+            return dp[i][amount];
+        }
+
+        // Pick (same index because unlimited coins allowed)
+        int pick = 0;
+
+        if (coins[i] <= amount) {
+            pick = solve(i, amount - coins[i], coins, dp);
+        }
+
+        // Not Pick
+        int notPick = solve(i + 1, amount, coins, dp);
+
+        return dp[i][amount] = pick + notPick;
     }
 }
