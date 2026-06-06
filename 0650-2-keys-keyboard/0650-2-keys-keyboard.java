18cce1e63a019370fc26c20533266fc545ed857f
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-06-06 12:29:08 +0530

    Time: 1 ms (44.86%), Space: 42.5 MB (22.16%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,14 +1,25 @@
+import java.util.*;
+
 class Solution {
 
     public int minSteps(int n) {
-        return solve(n);
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return solve(n, dp);
     }
 
-    private int solve(int n) {
+    private int solve(int n, int[] dp) {
 
         // Base Case
         if (n == 1) {
             return 0;
+        }
+
+        // Already Computed
+        if (dp[n] != -1) {
+            return dp[n];
         }
 
         int ans = n; // worst case
@@ -19,11 +30,11 @@ class Solution {
 
                 ans = Math.min(
                     ans,
-                    solve(j) + (n / j)
+                    solve(j, dp) + (n / j)
                 );
             }
         }
 
-        return ans;
+        return dp[n] = ans;
     }
 }
