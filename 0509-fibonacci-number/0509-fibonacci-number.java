2b0c08de50322bfa1f740676b620dd03580419a2
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-06-07 14:28:56 +0530

    Time: 11 ms (9.17%), Space: 42 MB (68.45%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,36 +1,14 @@
 class Solution {
     public int fib(int n) {
-
-        // dp array
-        int[]dp=new int[n+1];
-
-        // initialized with -1
-        Arrays.fill(dp,-1);
-
-        return solve(n,dp);
-        
+        return solve(n);
     }
 
-    public int solve(int n,int[]dp){
+    private int solve(int n){
 
         // base case
-        if(n==0){
-            return 0;
-        }
+        if(n==0) return 0;
+        if(n==1) return 1;
 
-        // computed already
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-
-        if(n==1){
-            return 1;
-        }
-
-        int step1=solve(n-1,dp);
-
-        int step2=solve(n-2,dp);
-
-        return dp[n]=step1+step2;
+        return solve(n-1)+solve(n-2);
     }
 }
