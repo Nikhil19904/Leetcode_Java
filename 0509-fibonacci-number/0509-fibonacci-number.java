ee5009e5933845bc22b8ab077551282fcf057571
Nikhil Suryavanshi <110816592+Nikhil19904@users.noreply.github.com>
2026-06-07 14:33:07 +0530

    Time: 19 ms (9.17%), Space: 42.1 MB (50.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,25 @@
 class Solution {
     public int fib(int n) {
-        return solve(n);
+
+        // dp array
+        int[]dp=new int[n+1];
+
+        Arrays.fill(dp,-1);
+
+        return solve(n,dp);
     }
 
-    private int solve(int n){
+    private int solve(int n,int[]dp){
 
         // base case
         if(n==0) return 0;
         if(n==1) return 1;
 
-        return solve(n-1)+solve(n-2);
+        // already computed
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        return solve(n-1,dp)+solve(n-2,dp);
     }
 }
